162d6789a17c98cc1e350d423e80ec96c92fd2fb
Gunjan <gpt47knl2@gmail.com>
2020-09-09 21:38:22 +0530

    beginning of Homepage

--- Amazon Clone/amazon-clone/src/Header.js
+++ Amazon Clone/amazon-clone/src/Header.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 export default function Header() {
     return (
         <div className='header'>
             <img className='header_logo' src='https://pngimg.com/uploads/amazon/amazon_PNG11.png' />
-            
+
             <div className='header_search'>
                 <input className='header_searchInput ' type='text' />
                 <SearchIcon className="header_searchIcon"/>
@@ -14,16 +15,22 @@ export default function Header() {
             <div className='header_nav'>
                 <div className='header_option'>
                     <span className='header_optionLineOne'>Hello Guest</span>
-                    <span className='header_optionLineOne'>Sign In</span>
+                    <span className='header_optionLineTwo'>Sign In</span>
                 </div>
                 <div className='header_option'>
                     <span className='header_optionLineOne'>Returns</span>
-                    <span className='header_optionLineOne'>& Orders</span>
+                    <span className='header_optionLineTwo'>& Orders</span>
                 </div>
                 <div className='header_option'>
                     <span className='header_optionLineOne'>Your</span>
-                    <span className='header_optionLineOne'>Prime</span>
+                    <span className='header_optionLineTwo'>Prime</span>
                 </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className='header_optionLineTwo header_basketCount'>0</span>
+
+                </div>
+
 
             </div>
         </div>
---
+++ Amazon Clone/amazon-clone/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className='home'>
+            <div className='home_container'>//generally 2 underscores are used in BEM convention
+                <img src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/Xiaomi/Redmi_8A_Dual/7499/V176845577_IN_WLME_Redmi_8A_Dual_LandingPage_1500x600._CB406191253_.jpg" alt=""/>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
